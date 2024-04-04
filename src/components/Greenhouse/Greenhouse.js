cbd231bc57579fff6f49b3554fdddeaa01be4d94
YZZlD <max.verhulst@hotmail.com>
2024-04-04 12:59:29 -0600

    Finished problem

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,17 +1,19 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import {useTheme} from '../../context/ThemeContext.js';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
-
+  const {themeName} = useTheme()
+  const image = themeName == "day" ? dayImage : nightImage;
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+            src={image}
+            alt='greenhouse'
       />
       <LightSwitch />
       <ClimateStats />
